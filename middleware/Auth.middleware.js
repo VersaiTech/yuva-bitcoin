02fd8a6f1b4a9d8c334a1ad90348ce0fa383bbb8
JayLunagariya15 <jaylunagariya7@gmail.com>
2024-02-20 16:44:52 +0530

    token

--- middleware/Auth.middleware.js
+++ middleware/Auth.middleware.js
@@ -31,27 +31,27 @@
 //   };
 
 
-async function isActive(userId) {
-    try {
-        // console.log("userId", userId);
-        const member = await Member.findOne({ member_user_id: userId });
-        // console.log("member", member);
-        return member;
-    } catch (error) {
-        console.log("error in ValidMember", error);
-        throw error;
-    }
-}
+// async function isActive(userId) {
+//     try {
+//         // console.log("userId", userId);
+//         const member = await Member.findOne({ member_user_id: userId });
+//         // console.log("member", member);
+//         return member;
+//     } catch (error) {
+//         console.log("error in ValidMember", error);
+//         throw error;
+//     }
+// }
 
-async function isActiveAdmin(userId) {
-    try {
-        const admin = await Admin.findOne({ admin_user_id: userId });
-        return admin;
-    } catch (error) {
-        console.log("error in ValidMember", error);
-        throw error;
-    }
-}
+// async function isActiveAdmin(userId) {
+//     try {
+//         const admin = await Admin.findOne({ admin_user_id: userId });
+//         return admin;
+//     } catch (error) {
+//         console.log("error in ValidMember", error);
+//         throw error;
+//     }
+// }
 
 
 require('dotenv').config();
@@ -61,55 +61,143 @@ const Admin = require("../models/AdminModel");
 
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
-const ValidMember = async (req, res, next) => {
-    const authHeader = req.headers.authorization;
 
-    if (!authHeader) {   //|| !authHeader.startsWith('Bearer ')
-        return res.status(401).send("No token provided");
-    }
+// Below is running code for ValidMember()
+// const ValidMember = async (req, res, next) => {
+//     const authHeader = req.headers.authorization;
 
-    // const token = authHeader.split(' ')[1];
+//     if (!authHeader) {   //|| !authHeader.startsWith('Bearer ')
+//         return res.status(401).send("No token provided");
+//     }
 
-    // if (!token) {
-    //     return res.status(401).send("A token is required for authentication");
-    // }
+//     // const token = authHeader.split(' ')[1];
 
+//     // if (!token) {
+//     //     return res.status(401).send("A token is required for authentication");
+//     // }
+
+//     try {
+//         const decoded = jwt.verify(authHeader, JWT_SECRET_KEY);
+
+//         console.log(decoded);
+
+//         const userId = decoded.userId;
+//         const userType = decoded.userType;
+
+//         const verify = await isActive(userId); 
+
+//         if (!verify) {
+//             return res.status(403).json({
+//                 success: false,
+//                 message: "Member Not Found !",
+//             });
+//         }
+//         if (verify.userType !== userType) {
+//             return res.status(403).json({
+//                 success: false,
+//                 message: "Invalid User Type !",
+//             });
+//         }
+
+//         // Attach the user object to the request for further processing
+//         req.user = verify;
+//         req.userType = userType;
+//         next();
+//     } catch (err) {
+//         if (err.name === 'JsonWebTokenError') {
+//             return res.status(401).send("Invalid token");
+//         } else {
+//             console.log(err)
+//             return res.status(500).send("Internal Server Error");
+//         }
+//     }
+// };
+
+// Authentication process with user activity check
+const authenticateUser = async (username, password) => {
     try {
-        const decoded = jwt.verify(authHeader, JWT_SECRET_KEY);
-
-        console.log(decoded);
-
-        const userId = decoded.userId;
-        const userType = decoded.userType;
-
-        const verify = await isActive(userId); 
-
-        if (!verify) {
-            return res.status(403).json({
-                success: false,
-                message: "Member Not Found !",
-            });
-        }
-        if (verify.userType !== userType) {
-            return res.status(403).json({
-                success: false,
-                message: "Invalid User Type !",
-            });
-        }
-
-        // Attach the user object to the request for further processing
-        req.user = verify;
-        req.userType = userType;
-        next();
-    } catch (err) {
-        if (err.name === 'JsonWebTokenError') {
-            return res.status(401).send("Invalid token");
-        } else {
-            console.log(err)
-            return res.status(500).send("Internal Server Error");
-        }
+      const user = await Member.findOne({ member_user_id: username });
+  
+      if (!user) {
+        // User not found
+        return { error: "User not found" };
+      }
+  
+      if (!user.isActive) {
+        // Inactive user, return an error
+        return { error: "Inactive User" };
+      }
+  
+      // Perform password validation here (compare hashed password, etc.)
+  
+      // Generate token if the user is active and password is valid
+      const token = jwt.sign(
+        { userId: user.member_user_id, userType: user.userType },
+        JWT_SECRET_KEY,
+        { expiresIn: '1h' } // Adjust expiration time as needed
+      );
+  
+      return { token };
+    } catch (error) {
+      console.error('Error during authentication:', error);
+      return { error: "Internal Server Error" };
     }
-};
+  };
+  
+  // ValidMember middleware with user activity check
+  const ValidMember = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+  
+    if (!authHeader) {
+      return res.status(401).send("No token provided");
+    }
+  
+    try {
+      const decoded = jwt.verify(authHeader, JWT_SECRET_KEY);
+  
+      const userId = decoded.userId;
+      const userType = decoded.userType;
+  
+      const user = await Member.findOne({ member_user_id: userId });
+  
+      if (!user) {
+        return res.status(403).json({
+          success: false,
+          message: "Invalid User! Please contact support.",
+        });
+      }
+  
+      if (!user.isActive) {
+        return res.status(403).json({
+          success: false,
+          message: "Inactive User! Please contact support.",
+        });
+      }
+  
+      if (user.userType !== userType) {
+        return res.status(403).json({
+          success: false,
+          message: "Invalid User Type!",
+        });
+      }
+  
+      // Attach the user object to the request for further processing
+      req.user = user;
+      req.userType = userType;
+      next();
+    } catch (err) {
+      if (err.name === 'JsonWebTokenError') {
+        return res.status(401).send("Invalid token");
+      } else {
+        console.error(err);
+        return res.status(500).send("Internal Server Error");
+      }
+    }
+  };
+  
+
+
+
 
 const isAdmin = async (req, res, next) => {
     const authHeader = req.headers.authorization;
@@ -160,6 +248,24 @@ const isAdmin = async (req, res, next) => {
         }
     }
 };
+
+// Updated isActive function
+// const isActive = async (userId) => {
+//     try {
+//         const user = await Member.findOne({ member_user_id: userId });
+
+//         if (!user) {
+//             return false; // User not found
+//         }
+
+//         return user.isActive; // Assuming there is an isActive field in the Member model
+//     } catch (error) {
+//         console.error('Error checking user activity:', error);
+//         return false; // Return false in case of an error
+//     }
+// };
+
+
 
 module.exports = {
     ValidMember,
