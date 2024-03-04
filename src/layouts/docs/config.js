32a427c572f32bbcbf28a783dea61dc1810d366f
Deepanshu Soni <117809462+deep3001@users.noreply.github.com>
2024-03-04 11:00:05 +0530

    white paper md file update

--- src/layouts/docs/config.js
+++ src/layouts/docs/config.js
@@ -26,109 +26,111 @@ export const sections = [
         path: paths.docs.deployment
       },
       {
-        title: 'Routing',
+        title: ' Proposition',
         path: paths.docs.routing
       },
       {
-        title: 'Theming',
+        title: 'Market Analytics',
+        // path: paths.docs.analytics
         path: paths.docs.theming
       },
       {
-        title: 'Redux',
+        title: 'Market Potential',
         path: paths.docs.redux
       },
       {
-        title: 'Server Calls',
+        title: 'Marketing Strategy',
+        
         path: paths.docs.serverCalls
       },
       {
-        title: 'Settings',
+        title: 'Utility',
         path: paths.docs.settings
       },
       {
-        title: 'RTL',
+        title: 'Legal Disclaimer',
         path: paths.docs.rtl
       },
-      {
-        title: 'Mapbox',
-        path: paths.docs.mapbox
-      },
-      {
-        title: 'Internationalization',
-        path: paths.docs.internationalization
-      }
+      // {
+      //   title: 'Mapbox',
+      //   path: paths.docs.mapbox
+      // },
+      // {
+      //   title: 'Internationalization',
+      //   path: paths.docs.internationalization
+      // }
     ]
   },
-  {
-    subheader: 'Authentication',
-    items: [
-      {
-        title: 'Amplify',
-        path: paths.docs.auth.amplify
-      },
-      {
-        title: 'Auth0',
-        path: paths.docs.auth.auth0
-      },
-      {
-        title: 'Firebase',
-        path: paths.docs.auth.firebase
-      },
-      {
-        title: 'JWT',
-        path: paths.docs.auth.jwt
-      }
-    ]
-  },
-  {
-    subheader: 'Guards',
-    items: [
-      {
-        title: 'Auth Guard',
-        path: paths.docs.guards.auth
-      },
-      {
-        title: 'Guest Guard',
-        path: paths.docs.guards.guest
-      },
-      {
-        title: 'Role Based Guard',
-        path: paths.docs.guards.roleBased
-      }
-    ]
-  },
-  {
-    subheader: 'Analytics',
-    items: [
-      {
-        title: 'Introduction',
-        path: paths.docs.analytics.introduction
-      },
-      {
-        title: 'Configuration',
-        path: paths.docs.analytics.configuration
-      },
-      {
-        title: 'Event Tracking',
-        path: paths.docs.analytics.eventTracking
-      }
-    ]
-  },
-  {
-    subheader: 'Support',
-    items: [
-      {
-        title: 'Changelog',
-        path: paths.docs.changelog
-      },
-      {
-        title: 'Contact',
-        path: paths.docs.contact
-      },
-      {
-        title: 'Further Support',
-        path: paths.docs.furtherSupport
-      }
-    ]
-  }
+  // {
+  //   subheader: 'Authentication',
+  //   items: [
+  //     {
+  //       title: 'Amplify',
+  //       path: paths.docs.auth.amplify
+  //     },
+  //     {
+  //       title: 'Auth0',
+  //       path: paths.docs.auth.auth0
+  //     },
+  //     {
+  //       title: 'Firebase',
+  //       path: paths.docs.auth.firebase
+  //     },
+  //     {
+  //       title: 'JWT',
+  //       path: paths.docs.auth.jwt
+  //     }
+  //   ]
+  // },
+  // {
+  //   subheader: 'Guards',
+  //   items: [
+  //     {
+  //       title: 'Auth Guard',
+  //       path: paths.docs.guards.auth
+  //     },
+  //     {
+  //       title: 'Guest Guard',
+  //       path: paths.docs.guards.guest
+  //     },
+  //     {
+  //       title: 'Role Based Guard',
+  //       path: paths.docs.guards.roleBased
+  //     }
+  //   ]
+  // },
+  // {
+  //   subheader: 'Analytics',
+  //   items: [
+  //     {
+  //       title: 'Introduction',
+  //       path: paths.docs.analytics.introduction
+  //     },
+  //     {
+  //       title: 'Configuration',
+  //       path: paths.docs.analytics.configuration
+  //     },
+  //     {
+  //       title: 'Event Tracking',
+  //       path: paths.docs.analytics.eventTracking
+  //     }
+  //   ]
+  // },
+  // {
+  //   subheader: 'Support',
+  //   items: [
+  //     {
+  //       title: 'Changelog',
+  //       path: paths.docs.changelog
+  //     },
+  //     {
+  //       title: 'Contact',
+  //       path: paths.docs.contact
+  //     },
+  //     {
+  //       title: 'Further Support',
+  //       path: paths.docs.furtherSupport
+  //     }
+  //   ]
+  // }
 ];
