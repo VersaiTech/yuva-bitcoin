36b4ebb36d4f5ad4ca47e7d02b05f25e8e7d6ee7
JayLunagariya15 <jaylunagariya7@gmail.com>
2024-03-16 21:38:42 +0530

    getallorders

--- controllers/Order.Controller.js
+++ controllers/Order.Controller.js
@@ -2,6 +2,7 @@ const Order = require('../models/Order');
 const Member = require('../models/memberModel');
 const Admin = require('../models/AdminModel');
 const TransactionHistory = require('../models/Transaction');
+const Joi = require('joi');
 
 // const createOrder = async (req, res) => {
 //     try {
@@ -127,7 +128,7 @@ const createOrder = async (req, res) => {
         const transactionHistory = new TransactionHistory({
             orderId: order._id,
             userId: member.member_user_id,
-            adminId: admin.admin_user_id,   
+            adminId: admin.admin_user_id,
             coin,
             amount,
             transactionType: 'order_sell' // This indicates it's a transaction related to an order
@@ -144,7 +145,52 @@ const createOrder = async (req, res) => {
 };
 
 
+const getAllOrder = async (req, res) => {
+    const Schema = Joi.object({
+        page_number: Joi.number(),
+        count: Joi.number(),
+    });
+    const { error, value } = Schema.validate(req.params);
+
+    if (error) {
+        return res.status(400).json({ status: false, error: error.details[0].message });
+    }
+    try {
+        const page_number = value.page_number || 1;
+        const count = value.count || 10;
+        const offset = (page_number - 1) * count;
+        const toalOrders = await Order.find();
+        // Fetch tasks for the user with sorting and pagination
+        const order = await Order.find()
+            .sort({ createdAt: -1 })
+            .skip(offset)
+            .limit(count);
+        // Find all orders
+        const orders = await Order.find();
+        if (!order || order.length === 0) {
+            return res.status(200).json({
+                status: false,
+                message: "No order ",
+                toalOrders,
+                order: [],
+            });
+        }
+
+        return res.status(200).json({
+            status: true,
+            message: "Orders Found",
+            totalOrders: toalOrders.length,
+            order: order,
+
+        });
+    } catch (error) {
+        console.error('Error fetching orders:', error);
+        res.status(500).json({ error: 'Failed to fetch orders' });
+    }
+};
+
+
 
 module.exports = {
-    createOrder
+    createOrder, getAllOrder,
 };
--- routes/Order.route.js
+++ routes/Order.route.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {createOrder} = require("../controllers/Order.Controller");
+const {createOrder,getAllOrder} = require("../controllers/Order.Controller");
 const { ValidMember, isAdmin } = require('../middleware/Auth.middleware');
 
 
 router.route("/createOrder").post(ValidMember, createOrder); //
-// router.route("/getAllOrders").get(isAdmin, getAllOrders);
-// router.route("/getAllMemberOrders").get(getAllOrders);
-// router.route("/getOneMembersOrders/:member_user_id").get(isAdmin, getOneMembersOrders);
-// router.route("/getAllOrdersUser").get(ValidMember, getAllOrdersUser);
+router.route("/getAllOrder/:page_number?/:count?").get(isAdmin, getAllOrder); 
 
 module.exports = router;
