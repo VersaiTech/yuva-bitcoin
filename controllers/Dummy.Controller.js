dc59dfcf0c510249179c0cad44a50dea2e6e0906
JayLunagariya15 <jaylunagariya7@gmail.com>
2024-04-12 09:35:30 +0530

    dummy

--- controllers/Dummy.Controller.js
+++ controllers/Dummy.Controller.js
@@ -9,12 +9,12 @@ const createDummyData = async (req, res) => {
             return res.status(400).json({ error: 'No admin found' });
         }
 
-        const { totalRegisteredMembers, totalCoinHolders, totalStakedCoins, totalSellTodayUSDT, totalSellTodayYuva, totalBuyTodayUSDT, totalBuyTodayYuva, totalUSDT, totalYuva } = req.body
+        // const { totalRegisteredMembers, totalCoinHolders, totalStakedCoins, totalSellTodayUSDT, totalSellTodayYuva, totalBuyTodayUSDT, totalBuyTodayYuva, totalUSDT, totalYuva } = req.body
 
-        if (!totalRegisteredMembers || !totalCoinHolders || !totalStakedCoins || !totalSellTodayUSDT || !totalSellTodayYuva || !totalBuyTodayUSDT || !totalBuyTodayYuva || !totalUSDT || !totalYuva) {
-            return res.status(400).json({ error: 'All fields are required ' })
-        }
-        console.log(req.body)
+        // if (!totalRegisteredMembers || !totalCoinHolders || !totalStakedCoins || !totalSellTodayUSDT || !totalSellTodayYuva || !totalBuyTodayUSDT || !totalBuyTodayYuva || !totalUSDT || !totalYuva) {
+        //     return res.status(400).json({ error: 'All fields are required ' })
+        // }
+        // console.log(req.body)
 
         const dummy = await Dummy.findOne();
         if (dummy) {
