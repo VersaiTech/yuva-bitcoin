b86a689333c350ef3b260adaeceb9c8351cbe710
iambhavesh09 <bhaveshjhawar09@gmail.com>
2024-05-21 15:19:44 +0530

    Sort By Removal

--- src/sections/dashboard/work-task/work-task-list-search.js
+++ src/sections/dashboard/work-task/work-task-list-search.js
@@ -162,23 +162,6 @@ export const WorkTaskSearch = (props) => {
             )}
           />
         </Box>
-        <TextField
-          label="Sort By"
-          name="sort"
-          onChange={handleSortChange}
-          select
-          SelectProps={{ native: true }}
-          value={`${sortBy}|${sortDir}`}
-        >
-          {sortOptions.map((option) => (
-            <option
-              key={option.value}
-              value={option.value}
-            >
-              {option.label}
-            </option>
-          ))}
-        </TextField>
       </Stack>
     </>
   );
