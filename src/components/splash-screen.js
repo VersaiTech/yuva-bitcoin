287d4871b13c6c8f19776d1a65a2d41b1eed7b47
iambhavesh09 <bhaveshjhawar09@gmail.com>
2024-05-21 13:08:27 +0530

    Favicon

--- src/components/splash-screen.js
+++ src/components/splash-screen.js
@@ -1,38 +1,3 @@
-// import { Box } from '@mui/material';
-// import Image from 'next/image';
-
-// export const SplashScreen = () => (
-//   <Box
-//     sx={{
-//       alignItems: 'center',
-//       backgroundColor: 'background.paper',
-//       display: 'flex',
-//       flexDirection: 'column',
-//       height: '100vh',
-//       justifyContent: 'center',
-//       left: 0,
-//       p: 3,
-//       position: 'fixed',
-//       top: 0,
-//       width: '100vw',
-//       zIndex: 1400
-//     }}
-//   >
-//     <Box
-//       sx={{
-//         display: 'inline-flex',
-//         height: 48,
-//         width: 48
-//       }}
-//     >
-//     <Image src="/assets/logo/yuvalogo.png" alt="Yuva Logo" width={100} height={100} />
-//     </Box>
-//   </Box>
-// );
-
-
-
-
 
 import { Box } from '@mui/material';
 import Image from 'next/image';
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,15 +31,6 @@ const Fonts = () => (
 const Favicon = () => (
   <>
     <link
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="/apple-touch-icon.png"
-    />
-    <link
-      rel="icon"
-      href="/favicon.ico"
-    />
-    <link
       rel="icon"
       type="image/png"
       sizes="32x32"
